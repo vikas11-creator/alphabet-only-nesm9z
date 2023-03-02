f941ceecc69a602bc14507de9dd0b42154cb70f2
vikas11-creator <82199478+vikas11-creator@users.noreply.github.com>
2023-03-02 11:30:02 +0530

    1 file modified

--- src/app/alphabet-only.directive.ts
+++ src/app/alphabet-only.directive.ts
@@ -1,16 +1,24 @@
 import { Directive, HostListener, ElementRef } from '@angular/core';
 
 @Directive({
-  selector: '[appAlphabetOnly]'
+  selector: '[appAlphabetOnly]',
 })
-
 export class AlphabetOnlyDirective {
   key;
   @HostListener('keydown', ['$event']) onKeydown(event: KeyboardEvent) {
     this.key = event.keyCode;
     console.log(this.key);
-    if ((this.key >= 15 && this.key <= 64) || (this.key >= 123) || (this.key >= 96 && this.key <= 105)) {
+    if (
+      (this.key >= 15 && this.key <= 64) ||
+      this.key >= 123 ||
+      (this.key >= 96 && this.key <= 105)
+    ) {
       event.preventDefault();
     }
   }
 }
+
+//https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/keyCode
+// check this for key code
+// A - Z === 65 - 90
+// 0 - 1 - 9 === 48 - 49 - 57
